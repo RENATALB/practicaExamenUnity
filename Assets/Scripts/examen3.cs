9376177eab652247cda41d7ef6fcc4735b1d33b3
RENATALB <166546709+RENATALB@users.noreply.github.com>
2025-04-26 15:02:51 -0300

    examen 3 terminado, empezando 6

--- Assets/Scripts/examen3.cs
+++ Assets/Scripts/examen3.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class examen3 : MonoBehaviour
 {
+    public string codigoMascota;
+    public int diasAQuedarse;
+    public int precioAlimento = 80;
+    int totalGramos;
+    float costoAlimento;
+
+    int gramosAlimentoPorDia;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (diasAQuedarse < 3 || codigoMascota != "G" || codigoMascota != "PP" || codigoMascota != "PG")
+        {
+            print("Formato inválido");
+        }
+        else if (codigoMascota == "G")
+        {
+            gramosAlimentoPorDia = 300;
+        }
+        else if (codigoMascota == "PP")
+        {
+            gramosAlimentoPorDia = 400;
+        }
+        else if (codigoMascota == "PG")
+        {
+            gramosAlimentoPorDia = 700;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+        totalGramos = gramosAlimentoPorDia * diasAQuedarse;
+        costoAlimento = totalGramos / 100 * 80;
+
+        Debug.Log("Para ese período se necesitan " + totalGramos + "gramos de alimento");
+
+        Debug.Log("Costo de esa cantidad de alimento: $" + costoAlimento);
+
+
     }
 }
---
+++ Assets/Scripts/examen6.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Schema;
+using UnityEngine;
+
+public class examen6 : MonoBehaviour
+{
+    public int entradaCampo;
+    public int entradaPlatea;
+    int recaudacionCampo;
+    int recaudacionPlatea;
+    int total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        recaudacionCampo = entradaCampo * 1200;
+        recaudacionPlatea = entradaPlatea * 2000;
+
+        total = recaudacionCampo + recaudacionPlatea;
+        print("La recaudacion total es: " + total);
+
+
+        if (entradaCampo == 20400 || entradaPlatea == 16200)
+        {
+            print("¡Sold Out!");
+        }
+
+    }
+}
